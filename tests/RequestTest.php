00688ad904e863b26281af59e1eb9b0714f84caf
Roman Savuliak <silwerclaw@gmail.com>
2016-07-24 20:45:13 +0300

    added phpunit simple config

---
+++ tests/RequestTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: silwerclaw
+ * Date: 24.07.16
+ * Time: 20:38
+ */
+class RequestTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function it_should_make_phpunit_pass()
+    {
+        $this->assertTrue(true);
+    }
+
+}
